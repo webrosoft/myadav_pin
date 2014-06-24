0c5bbf5c7bcfb8faceb03ca6fc95e77d0e8d4d36
manoj <myadav@adrindia.org>
2014-06-24 13:08:41 +0530

    Initially adding a page file

---
+++ page.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+if($_SESSION['user_name']=='')
+		{
+			header("location:index.php");
+		}
+
+function sendPage($body)
+{
+
+$html = <<< EOD
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Tool  for Updating Pincode With Constituency</title>
+</head>
+
+<body>
+$body
+</body>
+</html>
+
+EOD;
+
+echo $html;
+}
+?>
