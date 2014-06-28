7c54d7f5bedd0dd15a5dd0d884472202bac7af1b
manoj <myadav@adrindia.org>
2014-06-28 13:15:26 +0530

    Add function

--- searchbox.php
+++ searchbox.php
@@ -1,19 +1,13 @@
 <?php
 include('connection.php');
+require_once 'page.php';
 if($_SESSION['user_name']=='')
 		{
 			header("location:index.php");
 		}
 
- ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Tool  for Updating Pincode With Constituency</title>
-</head>
 
-<body>
+$body= <<< EOD
 <center>
 <h1>Tool  For Updating Pin Code Information</h1>
 <form name="edit_pin" action="search1.php" method="post">
@@ -22,5 +16,6 @@ if($_SESSION['user_name']=='')
 <input type="submit" name="submit" value="View Info" />
 </form>
 </center>
-</body>
-</html>
+EOD;
+sendPage($body);
+?>
